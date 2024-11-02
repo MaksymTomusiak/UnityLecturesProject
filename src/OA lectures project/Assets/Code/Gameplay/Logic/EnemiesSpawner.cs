932fa42bbffe0e4742544b499286dc2ca402eae3
MaksymTomusiak <maksym.tomusiak@oa.edu.ua>
2024-11-02 23:34:47 +0200

    refactor: refactor enamies spawner to use coroutine

--- src/OA lectures project/Assets/Code/Gameplay/Logic/EnemiesSpawner.cs
+++ src/OA lectures project/Assets/Code/Gameplay/Logic/EnemiesSpawner.cs
@@ -1,28 +1,24 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Code.Gameplay.Logic
 {
     public class EnemiesSpawner : MonoBehaviour
     {
-        private float _timer;
-        
         [SerializeField]
         private float _spawnInterval = 2;
         
         [SerializeField]
         private GameObject _enemy;
 
-        private void Start() 
-            => _timer = _spawnInterval;
+        private void Start() => StartCoroutine(SpawnEnemyCoroutine());
 
-        private void Update()
+        private IEnumerator SpawnEnemyCoroutine()
         {
-            _timer -= Time.deltaTime;
-
-            if (_timer < 0)
+            while (true)
             {
+                yield return new WaitForSeconds(_spawnInterval);
                 SpawnEnemy();
-                _timer = _spawnInterval;
             }
         }
 
