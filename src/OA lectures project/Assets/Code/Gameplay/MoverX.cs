5543426e811565630dd1a9d54bd0edc450c70648
MaksymTomusiak <maksym.tomusiak@oa.edu.ua>
2024-11-02 22:21:50 +0200

    refactor: refactored player movement

--- src/OA lectures project/Assets/Code/Gameplay/MoverX.cs
+++ src/OA lectures project/Assets/Code/Gameplay/MoverX.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace Code
+namespace Code.Gameplay
 {
     internal class MoverX : MonoBehaviour
     {
@@ -8,11 +8,13 @@ namespace Code
         private Rigidbody2D _rigidbody;
         
         [SerializeField] 
-        private float speed;
+        private float _speed;
+
+        public float Speed => _rigidbody.velocity.x;
 
         public void Move(float input)
         {
-            _rigidbody.velocity = new Vector2(input * speed, _rigidbody.velocity.y);
+            _rigidbody.velocity = new Vector2(input * _speed, _rigidbody.velocity.y);
         }
     }
 }
--- src/OA lectures project/Assets/Code/Gameplay/PlayerInputX.cs
+++ src/OA lectures project/Assets/Code/Gameplay/PlayerInputX.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
 
-namespace Code
+namespace Code.Gameplay
 {
     public class PlayerInputX : MonoBehaviour
     {
+        private const string AxisHorizontal = "Horizontal";
+
         [SerializeField]
         private MoverX _mover;
         private void Update()
         {
-            float input = Input.GetAxis("Horizontal");
+            float input = Input.GetAxis(AxisHorizontal);
             _mover.Move(input);
         }
     }
