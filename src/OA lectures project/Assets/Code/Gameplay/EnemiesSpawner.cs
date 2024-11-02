c84f07d58663aea572b5e0a4ccc1755461cdaa3c
MaksymTomusiak <maksym.tomusiak@oa.edu.ua>
2024-11-02 23:22:54 +0200

    feat: enemies spawner implemented

---
+++ src/OA lectures project/Assets/Code/Gameplay/EnemiesSpawner.cs
@@ -0,0 +1,36 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Code.Gameplay
+{
+    public class EnemiesSpawner : MonoBehaviour
+    {
+        private float _timer;
+        
+        [SerializeField]
+        private float _spawnInterval = 2;
+        
+        [SerializeField]
+        private GameObject _enemy;
+
+        private void Start() 
+            => _timer = _spawnInterval;
+
+        private void Update()
+        {
+            _timer -= Time.deltaTime;
+
+            if (_timer < 0)
+            {
+                SpawnEnemy();
+                _timer = _spawnInterval;
+            }
+        }
+
+        private void SpawnEnemy()
+        {
+            Instantiate(_enemy, gameObject.transform);
+        }
+    }
+}
